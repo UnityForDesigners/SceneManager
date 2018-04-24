b9f9687d375fd07dbffca4c1cee03f2db5d7815d
UnityForDesigners <37074023+UnityForDesigners@users.noreply.github.com>
2018-04-24 14:21:46 +0200

    Initial commit

---
+++ Assets/My Scritps/ChangeSceneFromTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneFromTrigger : MonoBehaviour {
+
+	void OnTriggerEnter (Collider hit){
+
+		if (hit.tag == "Player") {
+			SceneManager.LoadScene ("Scene02"); 
+
+		}
+
+	}
+}
